501225fb71a5b80dadc2952ac291af08248303d8
MathiasSeguy-Android2EE <mathias.seguy@android2ee.com>
2019-06-05 18:00:17 +0200

    First commit after migration of may 2019

--- jobschedulersimple/src/main/java/com/android2ee/jobschedulersimple/MainActivity.java
+++ jobschedulersimple/src/main/java/com/android2ee/jobschedulersimple/MainActivity.java
@@ -1,7 +1,7 @@
 package com.android2ee.jobschedulersimple;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
